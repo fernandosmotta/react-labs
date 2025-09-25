234e4336e2e5e6bef759793fc00bdf8f9a82fe16
Fernando Soares <fernando.smotta@gmail.com>
2025-09-24 21:40:30 -0300

    Lista de supermercado

--- aula02-tarefa/src/App.jsx
+++ aula02-tarefa/src/App.jsx
@@ -1,6 +1,7 @@
 // Importação Padrão
 import TarefasSimples from "./pages/TarefasSimples"
 import TarefasComStatus from "./pages/TarefasComStatus"
+import ListaSupermercado from "./pages/ListaSupermercado";
 
 // Importação Nomeada (Quando importo mais de uma função)
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
@@ -15,12 +16,14 @@ function App() {
 
           <nav>
             <Link to="/" className="btn btn-outline-primary fw-bold me-3">Tarefas Simples</Link>
-            <Link to="/status" className="btn btn-outline-secondary fw-bold">Tarefas com Status</Link>
+            <Link to="/status" className="btn btn-outline-secondary fw-bold me-3">Tarefas com Status</Link>
+            <Link to="/lista-supermercado" className="btn btn-outline-secondary fw-bold">Lista de Supermercado</Link>
           </nav>
 
           <Routes>
             <Route path="/" element={<TarefasSimples />} />
             <Route path="/status" element={<TarefasComStatus />} />
+            <Route path="/lista-supermercado" element={<ListaSupermercado />} />
           </Routes>
 
         </div>
